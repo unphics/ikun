be0841bd2fe46cab81a2a6ab43a86d2521128c69
unphics <481381521@qq.com>
2024-03-30 17:32:52 +0800

    script input and default map

--- Ikun/Content/Script/Ikun/Blueprint/PlayerController/BP_IkunPC.lua
+++ Ikun/Content/Script/Ikun/Blueprint/PlayerController/BP_IkunPC.lua
@@ -7,8 +7,23 @@ local BP_IkunPC = UnLua.Class()
 -- function BP_IkunPC:UserConstructionScript()
 -- end
 
--- function BP_IkunPC:ReceiveBeginPlay()
--- end
+function BP_IkunPC:ReceiveBeginPlay()
+    ---@type UEnhancedInputLocalPlayerSubsystem
+    local EnhancedInputSystem = UE.USubsystemBlueprintLibrary.GetLocalPlayerSubSystemFromPlayerController(self, UE.UEnhancedInputLocalPlayerSubsystem)
+    if EnhancedInputSystem then
+        ---@type IMC_Test
+        local IMC_Test = UE.UObject.Load('/Game/Ikun/Blueprint/Input/IMC/IMC_Test.IMC_Test')
+        if IMC_Test and (not EnhancedInputSystem:HasMappingContext(IMC_Test)) then
+            EnhancedInputSystem:AddMappingContext(IMC_Test, 100, UE.FModifyContextOptions())
+        end
+
+        ---@type IMC_Default
+        local IMC_Default = UE.UObject.Load('/Game/Ikun/Blueprint/Input/IMC/IMC_Default.IMC_Default')
+        if IMC_Default and (not EnhancedInputSystem:HasMappingContext(IMC_Default)) then
+            EnhancedInputSystem:AddMappingContext(IMC_Default, 100, UE.FModifyContextOptions())
+        end
+    end
+end
 
 -- function BP_IkunPC:ReceiveEndPlay()
 -- end
@@ -24,5 +39,30 @@ local BP_IkunPC = UnLua.Class()
 
 -- function BP_IkunPC:ReceiveActorEndOverlap(OtherActor)
 -- end
+
+--[[
+    UE.UEnhancedInputLibrary.Conv_InputActionValueToBool
+    UE.UEnhancedInputLibrary.Conv_InputActionValueToAxis1D
+    UE.UEnhancedInputLibrary.Conv_InputActionValueToAxis2D
+    UE.UEnhancedInputLibrary.Conv_InputActionValueToAxis3D
+]]
+
+local EnhancedInput = require("UnLua.EnhancedInput")
+EnhancedInput.BindAction(BP_IkunPC, '/Game/Ikun/Blueprint/Input/IA/IA_Test.IA_Test', 'Triggered', 
+    function(SourceObj, ActionValue, ElapsedSeconds, TriggeredSeconds, InputAction)
+        log.warn('不知道为啥数值总是不对', ActionValue) -- UE.UEnhancedInputLibrary.Conv_InputActionValueToAxis2D(ActionValue)
+    end)
+
+local EnhancedInput = require("UnLua.EnhancedInput")
+EnhancedInput.BindAction(BP_IkunPC, '/Game/Ikun/Blueprint/Input/IA/IA_Move.IA_Move', 'Triggered', 
+    function(SourceObj, ActionValue, ElapsedSeconds, TriggeredSeconds, InputAction)
+        log.warn('move input', ActionValue.X, ActionValue.Y) -- UE.UEnhancedInputLibrary.Conv_InputActionValueToAxis2D(ActionValue)
+    end)
+
+local EnhancedInput = require("UnLua.EnhancedInput")
+EnhancedInput.BindAction(BP_IkunPC, '/Game/Ikun/Blueprint/Input/IA/IA_Look.IA_Look', 'Triggered', 
+    function(SourceObj, ActionValue, ElapsedSeconds, TriggeredSeconds, InputAction)
+        log.warn('look input', ActionValue.X, ActionValue.Y) -- UE.UEnhancedInputLibrary.Conv_InputActionValueToAxis2D(ActionValue)
+    end)
 
 return BP_IkunPC
