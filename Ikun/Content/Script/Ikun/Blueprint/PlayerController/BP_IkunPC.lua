7e49293060541d29dd54b85cc16e1895b8fbee1c
unphics <481381521@qq.com>
2024-04-03 11:19:21 +0800

    look input

--- Ikun/Content/Script/Ikun/Blueprint/PlayerController/BP_IkunPC.lua
+++ Ikun/Content/Script/Ikun/Blueprint/PlayerController/BP_IkunPC.lua
@@ -52,17 +52,26 @@ function BP_IkunPC:ReceivePossess(PossessedPawn)
 end
 
 EnhancedInput.BindAction(BP_IkunPC, '/Game/Ikun/Blueprint/Input/IA/IA_Move.IA_Move', 'Triggered', 
-function(SourceObj, ActionValue, ElapsedSeconds, TriggeredSeconds, InputAction)
-    -- log.warn('move input', ActionValue.X, ActionValue.Y)
-    local PC = SourceObj
-    local Rot = PC:GetControlRotation()
-    local YawRot = UE.FRotator(0, Rot.Yaw, 0)
-    local Forward = YawRot:GetForwardVector()
-    if PC.OwnerChr then
-        PC.OwnerChr:MoveForwardBack(Forward, ActionValue.Y)
-        PC.OwnerChr:MoveRightLeft(Forward, ActionValue.X)
-    end
-end)
+    function(SourceObj, ActionValue, ElapsedSeconds, TriggeredSeconds, InputAction)
+        -- log.warn('move input', ActionValue.X, ActionValue.Y)
+        local PC = SourceObj
+        local Rot = PC:GetControlRotation()
+        local YawRot = UE.FRotator(0, Rot.Yaw, 0)
+        local Forward = YawRot:GetForwardVector()
+        if PC.OwnerChr then
+            PC.OwnerChr:MoveForwardBack(Forward, ActionValue.Y)
+            PC.OwnerChr:MoveRightLeft(Forward, ActionValue.X)
+        end
+    end)
+EnhancedInput.BindAction(BP_IkunPC, '/Game/Ikun/Blueprint/Input/IA/IA_Look.IA_Look', 'Triggered', 
+    function(SourceObj, ActionValue, ElapsedSeconds, TriggeredSeconds, InputAction)
+        local PC = SourceObj
+        PC:AddYawInput(ActionValue.X * 50 * UE.UGameplayStatics.GetWorldDeltaSeconds(PC))
+        PC:AddPitchInput(ActionValue.Y * 50 * UE.UGameplayStatics.GetWorldDeltaSeconds(PC))
+    end)
+
+
+return BP_IkunPC
 
 --[[
     UE.UEnhancedInputLibrary.Conv_InputActionValueToBool
@@ -88,4 +97,3 @@ EnhancedInput.BindAction(BP_IkunPC, '/Game/Ikun/Blueprint/Input/IA/IA_Look.IA_Lo
     end)
 ]]
 
-return BP_IkunPC
