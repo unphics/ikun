0c1852299533117fb61da32ad660b776ddcff1b5
unphics <481381521@qq.com>
2024-06-20 18:02:50 +0800

    搞了一些Jump的StateMachine，但是还没有处理按键触发

---
+++ Ikun/Content/Script/Ikun/Chr/GostSamurai/BP_GostSamurai.lua
@@ -0,0 +1,39 @@
+--
+-- DESCRIPTION
+--
+-- @COMPANY **
+-- @AUTHOR **
+-- @DATE ${date} ${time}
+--
+local ERotMode = UE.UObject.Load('/Game/Ikun/Anim/Blueprint/Enum/RotMode.RotMode')
+
+---@type BP_GhostSamurai_C
+local M = UnLua.Class()
+
+-- function M:Initialize(Initializer)
+-- end
+
+-- function M:UserConstructionScript()
+-- end
+
+function M:ReceiveBeginPlay()
+
+    self.AnimComp.RotMode = ERotMode.LookDir
+end
+
+-- function M:ReceiveEndPlay()
+-- end
+
+-- function M:ReceiveTick(DeltaSeconds)
+-- end
+
+-- function M:ReceiveAnyDamage(Damage, DamageType, InstigatedBy, DamageCauser)
+-- end
+
+-- function M:ReceiveActorBeginOverlap(OtherActor)
+-- end
+
+-- function M:ReceiveActorEndOverlap(OtherActor)
+-- end
+
+return M
