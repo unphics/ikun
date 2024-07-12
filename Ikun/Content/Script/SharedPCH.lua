84bf828fe1c78e83f97e17923cbacff8539060d3
unphics <481381521@qq.com>
2024-07-12 16:35:26 +0800

    双边渐隐材质和投射纹理材质

---
+++ Ikun/Content/Script/SharedPCH.lua
@@ -0,0 +1,15 @@
+--[[
+    Shared Precompiled Header
+]]
+require('Util.Debug.Log')
+class = require("Util.Class.class1")
+require("Util.Class.test1")
+
+require('Util.Gas.GasUtil')
+require('Util.Actor.ActorUtil')
+require('Util.AI.DecisionUtil')
+require('Util.Obj.ObjUtil')
+require("Util.Net.NetUitl")
+require("Util.World.WorldUtil")
+
+require('Ikun.Module.MdMgr')
