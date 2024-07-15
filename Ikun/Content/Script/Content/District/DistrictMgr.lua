5f5f2e23b6a7dbb847e77259825c552d1b4fbfae
unphics <481381521@qq.com>
2024-07-15 20:27:12 +0800

    行政区划，未完

--- Ikun/Content/Script/Content/District/DistrictMgr.lua
+++ Ikun/Content/Script/Content/District/DistrictMgr.lua
@@ -2,6 +2,10 @@
     @brief 行政区划管理器
 ]]
 
+require("Content/District/Kingdom")
+require("Content/District/Zone")
+require("Content/District/Settlements/Settlement")
+
 class.class"DistrictMgr" : extends "MdBase" {
     ctor = function(self)
     end,
