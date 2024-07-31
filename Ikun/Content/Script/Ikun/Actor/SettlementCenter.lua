6d4cb50e68ce672a37a929edbaa5f8e87f8fafc8
unphics <481381521@qq.com>
2024-07-31 14:12:24 +0800

    测试NPC头顶对话框

--- Ikun/Content/Script/Ikun/Actor/SettlementCenter.lua
+++ Ikun/Content/Script/Ikun/Actor/SettlementCenter.lua
@@ -11,6 +11,12 @@ local CfgSettlement = require("Content.District.Config.Settlement")
 ---@type SettlementCenter
 local M = UnLua.Class()
 
+---@private 测试SizeToContent的TextBlock的换行
+local TestTextWrap = function(self, DeltaTime)end
+
+---@public [Init] 初始化人类聚集地
+M.InitSettlement = function(self)end
+
 -- function M:Initialize(Initializer)
 -- end
 
@@ -38,7 +44,7 @@ function M:ReceiveTick(DeltaSeconds)
         self.WidgetComp:K2_SetRelativeRotation(Rot, false, UE.FHitResult(), false)
 
         -- TODO Test TopMark WidgetComp测试
-        -- self.WidgetComp:GetWidget().TxtName:SetText("请问如何iOS吧覅哦为方便五哦啊是发表欧维设备嗲数量的报复为方便")
+        -- TestTextWrap(self, DeltaSeconds)
     end
 end
 
@@ -51,15 +57,41 @@ end
 -- function M:ReceiveActorEndOverlap(OtherActor)
 -- end
 
-function M:InitSettlement()
+function M:SetTopMarkName_RPC(Name)
+    self.WidgetComp:GetWidget().TxtName:SetText(Name)
+end
+
+M.InitSettlement = function(self)
     -- register settlement ue entity to content module
     local SettlementLua = MdMgr.tbMd.ConMgr.tbCon.AreaMgr:GetStar().DistrictMgr:GetKingdom(CfgSettlement[self.SettlementId].Kingdom):FindSettlementLua(self.SettlementId)
     SettlementLua.Actor = self
     self:SetTopMarkName(SettlementLua.SettlementName)
 end
 
-function M:SetTopMarkName_RPC(Name)
-    self.WidgetComp:GetWidget().TxtName:SetText(Name)
+TestTextWrap = function(self, DeltaTime)
+    if not self.count then
+        self.count = 0
+    end
+    self.count = self.count + DeltaTime
+    if self.count > 2 then
+        self.count = 0
+        if self.index == 0 then
+            self.WidgetComp:GetWidget().TxtName:SetText("陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆")
+            self.index = 1
+        elseif self.index == 1 then
+            self.WidgetComp:GetWidget().TxtName:SetText("陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆陆")
+            self.index = 2
+        elseif self.index == 2 then
+            self.WidgetComp:GetWidget().TxtName:SetText("陆陆陆陆")
+            self.index = 3
+        elseif self.index == 3 then
+            self.WidgetComp:GetWidget().TxtName:SetText("陆")
+            self.index = 4
+        else
+            self.WidgetComp:GetWidget().TxtName:SetText("陆陆陆陆陆陆陆陆陆")
+            self.index = 0
+        end
+    end
 end
 
 return M
