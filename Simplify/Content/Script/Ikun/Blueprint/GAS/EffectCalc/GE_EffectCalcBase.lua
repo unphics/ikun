1941594278000da335df52de7903f4117abe9819
unphics <481381521@qq.com>
2025-06-07 22:50:44 +0800

    小修改

--- Simplify/Content/Script/Ikun/Blueprint/GAS/EffectCalc/GE_EffectCalcBase.lua
+++ Simplify/Content/Script/Ikun/Blueprint/GAS/EffectCalc/GE_EffectCalcBase.lua
@@ -8,28 +8,7 @@
 ---@class GE_EffectCalcBase: GE_EffectCalcBase_C
 local GE_EffectCalcBase = UnLua.Class()
 
--- function GE_EffectCalcBase:OnCalcEffectData()
---     local ContextHandle = self.Spec.EffectContext
---     local Ability = UE.UIkunFnLib.EffectContextGetAbility(self.Spec.EffectContext)
-
---     local PhyDmgCfgCorr = 1
-
---     local SourceAttackPowerVal = self:ReadAttrValue('AttackPower', true)
-    
---     local TargetPhysicalDefenseVal = self:ReadAttrValue('PhysicalDefense', false)
---     local PercentPhysicalDefenseVal = math.log(TargetPhysicalDefenseVal) -- log的底数为nil的话默认是e
---     local PhyDmgVal = SourceAttackPowerVal * PhyDmgCfgCorr * (1 - PercentPhysicalDefenseVal)
-
---     log.dev('GE_EffectCalcBase:OnCalcEffectData qqqq', SourceAttackPowerVal, PhyDmgCfgCorr, PercentPhysicalDefenseVal, PhyDmgVal)
-    
-    
---     local TargetHealthVal = self:ReadAttrValue('Health', false)
-    
---     log.dev('GE_EffectCalcBase:OnCalcEffectData', TargetHealthVal)
-
---     self:ModiAttrValue('Health', -3, UE.EGameplayModOp.Additive)
--- end
-
+---@override
 ---@param InParams FGameplayEffectCustomExecutionParameters
 ---@param OutParams FGameplayEffectCustomExecutionOutput
 function GE_EffectCalcBase:Execute(InParams, OutParams)
@@ -37,16 +16,25 @@ function GE_EffectCalcBase:Execute(InParams, OutParams)
     local CalcObj = UE.NewObject(CalcObjClass, self) ---@type BP_EffectCalcObj_C
     CalcObj:InitEffectCalcData(InParams)
     local Ability = UE.UIkunFnLib.EffectContextGetAbility(CalcObj.Spec.EffectContext)
+
+    self:OnExecute(CalcObj, Ability)
+    
+    OutParams = CalcObj.OutExecParams
+end
+
+---@protected
+---@param CalcObj BP_EffectCalcObj_C
+---@param Ability GA_IkunBase
+function GE_EffectCalcBase:OnExecute(CalcObj, Ability)
     local PhyDmgCfgCorr = 1
     local SrcAttackPowerVal = CalcObj:ReadAttrValue('AttackPower', true)
     local TarPhyDefVal = CalcObj:ReadAttrValue('PhysicalDefense', false)
     local PercPhyDefVal = math.log(TarPhyDefVal) -- log的底数为nil的话默认是e
     local PhyDmgVal = SrcAttackPowerVal * PhyDmgCfgCorr * (1 - PercPhyDefVal)
-    log.dev(' GE_EffectCalcBase:Execute ', SrcAttackPowerVal, PhyDmgCfgCorr, PercPhyDefVal, PhyDmgVal)
+    log.dev('GE_EffectCalcBase:Execute ', SrcAttackPowerVal, PhyDmgCfgCorr, PercPhyDefVal, PhyDmgVal)
     local TarHealthVal = CalcObj:ReadAttrValue('Health', false)
     log.dev('GE_EffectCalcBase:Execute', TarHealthVal)
     CalcObj:ModiAttrValue('Health', -3, UE.EGameplayModOp.Additive)
-    OutParams = CalcObj.OutExecParams
 end
 
 return GE_EffectCalcBase
