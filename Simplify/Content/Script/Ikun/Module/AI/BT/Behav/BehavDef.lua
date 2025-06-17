fa4f5420f2d2cd5adf8a79c3e06be8ebcd4273b0
unphics <481381521@qq.com>
2025-06-17 22:28:45 +0800

    tmp

---
+++ Simplify/Content/Script/Ikun/Module/AI/BT/Behav/BehavDef.lua
@@ -0,0 +1,18 @@
+
+---
+---@brief 行为定义
+---@author zys
+---@data Mon Jun 16 2025 21:56:40 GMT+0800 (中国标准时间)
+---
+
+---@enum BehavDef
+local BehavDef = {
+    Special = '特殊行为',
+    Support = '支援行为',
+    Disturb = '骚扰行为',
+    Defense = '防御行为',
+    Damage = '伤害行为',
+    Move = '移动行为',
+}
+
+return BehavDef
