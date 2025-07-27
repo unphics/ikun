413e8cc5b4af4d0e700741644f175ece8c8fefac
unphics <481381521@qq.com>
2025-07-27 18:16:04 +0800

    小修改

--- Simplify/Content/Script/Ikun/Chr/Archer/ABP_Archer.lua
+++ Simplify/Content/Script/Ikun/Chr/Archer/ABP_Archer.lua
@@ -47,7 +47,7 @@ end
 
 ---@private 更新地面移动有关信息
 function ABP_Archer:UpdateMoveSpeed(DeltaTime)
-    do -- 速度的标量大小
+    do -- 计算速度的标量大小
         local worldVel = self.Chr:GetVelocity()
         worldVel.Z = 0
         self.Speed = UE.UKismetMathLibrary.VSize(worldVel)
@@ -58,8 +58,6 @@ function ABP_Archer:UpdateMoveSpeed(DeltaTime)
         local worldRot = self.Chr:K2_GetActorRotation()
         local worldVelDir = UE.UKismetMathLibrary.Normal(worldVel, 0.0001) -- tolerance:公差
         local relVelDir = UE.UKismetMathLibrary.LessLess_VectorRotator(worldVelDir, worldRot) ---@type FVector 将世界空间的速度转成角色本地空间的速度
-    
-        -- log.dev('qqq worldVelDir', worldVelDir)
 
         local velSumCount = math.abs(relVelDir.X) + math.abs(relVelDir.Y) + math.abs(relVelDir.Z)
         local velDirPer = relVelDir / velSumCount ---@type FVector 归一化的相对速度, 每个值都是百分比占比量
@@ -70,22 +68,15 @@ function ABP_Archer:UpdateMoveSpeed(DeltaTime)
         velBelnd.L = math.abs(math_util.clamp(velDirPer.Y, -1, 0))
         velBelnd.R = math_util.clamp(velDirPer.Y, 0, 1)
 
-        -- log.dev('qqq', log.fmt54(velBelnd.F), log.fmt54(velBelnd.B), log.fmt54(velBelnd.L),
-        --     log.fmt54(velBelnd.R))
-    
         ---@step 2.将当前混合速度插值到此时的瞬时混合速度
-
         local interp_speed = 50
         self.VelBlend.F = UE.UKismetMathLibrary.FInterpTo(self.VelBlend.F, velBelnd.F, DeltaTime, interp_speed)
         self.VelBlend.B = UE.UKismetMathLibrary.FInterpTo(self.VelBlend.B, velBelnd.B, DeltaTime, interp_speed)
         self.VelBlend.L = UE.UKismetMathLibrary.FInterpTo(self.VelBlend.L, velBelnd.L, DeltaTime, interp_speed)
         self.VelBlend.R = UE.UKismetMathLibrary.FInterpTo(self.VelBlend.R, velBelnd.R, DeltaTime, interp_speed)
-        
-        -- log.dev(log.key.abp, 'relVelDir', relVelDir)
-        -- log.dev(log.key.abp, 'velDirPer', velDirPer)
     end
 
-    do -- 计算移动方向
+    do -- 计算移动方向枚举值
         local aimRot = self.Chr:GetControlRotation()
         local worldVel = self.Chr:GetVelocity()
         local worldVelRot = UE.UKismetMathLibrary.Conv_VectorToRotator(worldVel)
