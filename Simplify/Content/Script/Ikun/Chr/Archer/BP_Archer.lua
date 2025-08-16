fd0d5e80cd2b0dd2f867560d7d0cab3cccacb8ac
unphics <481381521@qq.com>
2025-08-16 16:45:12 +0800

    小修改

--- Simplify/Content/Script/Ikun/Chr/Archer/BP_Archer.lua
+++ Simplify/Content/Script/Ikun/Chr/Archer/BP_Archer.lua
@@ -13,36 +13,14 @@ local BP_Archer = UnLua.Class('Ikun/Chr/Blueprint/BP_ChrBase')
 ---@override
 function BP_Archer:ReceiveBeginPlay()
     self.Super.ReceiveBeginPlay(self)
-    -- local a = function()
-    --     log.dev('qqq', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    -- end
-    -- if net_util.is_server(self) then
-    --     -- self.ASC.OnTagChanged:Add(self, self.OnASCTagChanged)
-    --     log.dev('qqq 0', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    --     self.ASC.OnTagChanged:Add(self, a)
-    -- end
-    -- if net_util.is_server(self) then
-    --     -- local tag = UE.UIkunFnLib.RequestGameplayTag('Input.MouseLeft.Completed')
-    --     -- local stopTask = UE.UAbilityTask_WaitGameplayTagAdded.WaitGameplayTagAdd(self, tag, nil, true)
-    --     -- stopTask.Added:Add(self, function()
-    --     --     log.dev('qqq 000', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))    
-    --     -- end)
-    --     -- stopTask:ReadyForActivation()
-    --     log.dev('qqq 1', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    --     gas_util.add_loose_tag(self, 'Input.MouseLeft.Completed')
-    --     log.dev('qqq 2', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    --     gas_util.remove_loose_tag(self, 'Input.MouseLeft.Completed')
-    --     log.dev('qqq 3', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    --     self.ASC.OnTagChanged:Remove(self, a)
-    --     gas_util.add_loose_tag(self, 'Input.MouseLeft.Completed')
-    --     log.dev('qqq 4', gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    -- end
+    -- self.ASC.OnTagChanged:Add(self,function(this, tag, bexist)
+    --     log.dev('[OnTagChanged]', tag.TagName, bexist)
+    -- end)
 end
 
 ---@override
 function BP_Archer:ReceiveTick(DeltaSeconds)
     self.Super.ReceiveTick(self, DeltaSeconds)
-    -- log.dev(gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'), net_util.print(self))
 end
 
 ---@public PC会调用此方法
@@ -59,19 +37,10 @@ function BP_Archer:C2S_LeftEnd_RPC()
         return
     end
     self.pull = false
-    gas_util.add_loose_tag(self, 'Input.MouseLeft.Completed')
-    log.dev('BP_Archer:C2S_LeftEnd_RPC()', net_util.print(self), gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
-    -- UE.UAbilitySystemBlueprintLibrary.SendGameplayEventToActor(self, UE.UIkunFnLib.RequestGameplayTag('Input.MouseLeft.Completed'), nil)
+    -- gas_util.add_loose_tag(self, 'Input.MouseLeft.Completed')
+    -- log.dev('BP_Archer:C2S_LeftEnd_RPC()', net_util.print(self), gas_util.has_loose_tag(self, 'Input.MouseLeft.Completed'))
+    UE.UAbilitySystemBlueprintLibrary.SendGameplayEventToActor(self, UE.UIkunFnLib.RequestGameplayTag('Input.MouseLeft.Completed'), nil)
 end
-
--- function BP_Archer:OnASCTagChanged(GameplayTag, bExist)
---     if GameplayTag.TagName == UE.UIkunFnLib.RequestGameplayTag('Skill.Tag.Shoot').TagName then
---         log.dev('qqqqqqqqqqqqqq')
---         local locBow = self.Bow:GetSocketLocation('Bow_String') -- self.Bow:GetSocketTransform('Bow_String', UE.ERelativeTransformSpace.RTS_World)
---         local rotChr = self:K2_GetActorRotation()
---         actor_util.spawn_always(self, ArrowClass, UE.FTransform(rotChr:ToQuat(), locBow))
---     end
--- end
 
 function BP_Archer:ArcherShoot()
     local locBow = self.Bow:GetSocketLocation('Bow_String') -- self.Bow:GetSocketTransform('Bow_String', UE.ERelativeTransformSpace.RTS_World)
--- Simplify/Content/Script/Ikun/Chr/Archer/Skill/Skill_01/GA_Archer_01.lua
+++ Simplify/Content/Script/Ikun/Chr/Archer/Skill/Skill_01/GA_Archer_01.lua
@@ -14,9 +14,13 @@ function GA_Archer_01:OnActivateAbility()
     self.Super.OnActivateAbility(self)
     log.dev('GA_Archer_01:OnActivateAbility()', net_util.print(self))
 
-    gas_util.has_loose_tag(self:GetAvatarActorFromActorInfo(), 'Input.MouseLeft.Completed')
+    ---@step check
+    if gas_util.has_loose_tag(self.AvatarLua, 'Input.MouseLeft.Completed') then
+        self:GAFail()
+        return log.error('GA_Archer_01:OnActivateAbility()', '右键的Tag没Consume', net_util.print(self))
+    end
 
-    ---@step montage play
+    ---@step 播放蒙太奇动作
     local at = UE.UATPlayMtgAndWaitEvent.PlayMtgAndWaitEvent(self, 'task name',
         self.MtgShoot, UE.FGameplayTagContainer(), 1, '', false, 1)
     at.OnBlendOut:Add(self, self.OnMtgCompleted)
@@ -24,24 +28,20 @@ function GA_Archer_01:OnActivateAbility()
     at.OnInterrupted:Add(self, self.OnMtgCancelled)
     at:ReadyForActivation()
     
-    ---@step wait mouse left completed
-    -- if net_util.is_server(self) then
+    ---@step 等待鼠标左键松开事件
+    if net_util.is_server(self) then
         local tag = UE.UIkunFnLib.RequestGameplayTag('Input.MouseLeft.Completed')
-        -- local stopTask= UE.UAbilityTask_WaitGameplayEvent.WaitGameplayEvent(self, tag, nil, true, true)
-        -- stopTask.EventReceived:Add(self, self.OnMouseLeftCompleted)
-        local stopTask = UE.UAbilityTask_WaitGameplayTagAdded.WaitGameplayTagAdd(self, tag, nil, true)
-        stopTask.Added:Add(self, self.OnMouseLeftCompleted)
+        local stopTask= UE.UAbilityTask_WaitGameplayEvent.WaitGameplayEvent(self, tag, nil, true, true)
+        stopTask.EventReceived:Add(self, self.OnMouseLeftCompleted)
+        -- local stopTask = UE.UAbilityTask_WaitGameplayTagAdded.WaitGameplayTagAdd(self, tag, nil, true)
+        -- stopTask.Added:Add(self, self.OnMouseLeftCompleted)
         stopTask:ReadyForActivation()
-    -- end
+    end
 
-    ---@step camera move
+    ---@step 相机切换到越肩视角
     if net_util.is_server(self) then
         local pc = UE.UGameplayStatics.GetPlayerController(self, 0)
         pc.CameraViewComp:S2C_NewCameraViewModel('Aim')
-    end
-
-    if gas_util.has_loose_tag(self.AvatarLua, 'Input.MouseLeft.Completed') then
-        log.error('GA_Archer_01:OnActivateAbility()', '右键的Tag没Consume', net_util.print(self))
     end
 end
 
@@ -60,34 +60,22 @@ function GA_Archer_01:OnMtgCancelled()
     self:GAFail()
 end
 
----@private [Server]
+---@private
 function GA_Archer_01:OnMouseLeftCompleted()
--- async_util.delay(self, 0.01, function()
-log.dev('GA_Archer_01:OnMouseLeftCompleted()', net_util.print(self), gas_util.has_loose_tag(self:GetAvatarActorFromActorInfo(), 'Input.MouseLeft.Completed'))
--- end)
-    ---@step consume tag
+    self:S2C_OnMouseLeftCompleted()
+end
 
-    if net_util.is_server(self) and gas_util.has_loose_tag(self:GetAvatarActorFromActorInfo(), 'Input.MouseLeft.Completed') then
-        -- gas_util.remove_loose_tag(self:GetAvatarActorFromActorInfo(), 'Input.MouseLeft.Completed')
-    end
-
-    -- if net_util.is_server(self) then
-    --     self:S2C_ConsumeTag()
-    -- end
-    
+---@private
+function GA_Archer_01:S2C_OnMouseLeftCompleted_RPC1()
+    log.dev('GA_Archer_01:OnMouseLeftCompleted()', net_util.print(self))
     ---@step montage jmp section
-    self:MontageJumpToSection('Aim_End')
+    -- self:MontageJumpToSection('Aim_End')
+    self.Overridden.S2C_OnMouseLeftCompleted(self)
     
-    ---@step camera move
+    ---@step 相机从越肩视角切换回普通视角
     if net_util.is_server(self) then
         local pc = UE.UGameplayStatics.GetPlayerController(self, 0)
         pc.CameraViewComp:S2C_NewCameraViewModel('Normal')
-    end
-end
-
-function GA_Archer_01:S2C_ConsumeTag_RPC()
-    if gas_util.remove_loose_tag(self.AvatarLua, 'Input.MouseLeft.Completed') then
-        gas_util.remove_loose_tag(self.AvatarLua, 'Input.MouseLeft.Completed')
     end
 end
 
