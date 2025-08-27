63da820def2c39198a781f6333c926aaf1e53e56
unphics <481381521@qq.com>
2025-08-28 01:48:02 +0800

    明天继续写Item

--- Simplify/Content/Script/Content/Item/Bag.lua
+++ Simplify/Content/Script/Content/Item/Bag.lua
@@ -5,35 +5,57 @@
 ---@data    Wed Aug 27 2025 00:45:13 GMT+0800 (中国标准时间)
 ---
 
-require('Content/Item/ItemBase')
-
 ---@class BagClass
----@field ItemContainer table<number, ItemBaseClass>
+---@field _ItemContainer table<number, ItemBaseClass>
+---@field _Owner RoleClass
 local BagClass = class.class 'BagClass' {
---[[public]]
     ctor = function()end,
---[[private]]
-    ItemContainer = nil,
+    AddItem = function()end,
+    RemoveItem = function()end,
+    _ItemContainer = nil,
+    _Owner = nil,
 }
 
-function BagClass:ctor()
-    self.ItemContainer = {}
+---@override
+function BagClass:ctor(Owner)
+    self._Owner = Owner
+    self._ItemContainer = {}
 end
 
+---@public
+---@param Item ItemBaseClass
 function BagClass:AddItem(Item)
+    local cfg = MdMgr.ConfigMgr:GetConfig('Item')[Item.ItemCfgId] ---@type ItemBaseConfig
+    self._ItemContainer[Item.ItemId] = Item
 end
 
-function BagClass:RemoveItem(ItemId)
-end
-
-function BagClass:MoveItemToAnother(AnotherBag, Item)
-    
-end
-
-function BagClass:GetItemById(ItemId)
-end
-
-function BagClass:GetItemByCfgId(ItemCfgId)
+---@public
+function BagClass:RemoveItem(ItemId, Count)
+    local item = self._ItemContainer[ItemId] ---@type ItemBaseClass
+    if not item then
+        return false
+    end
+    local cfgId = item.ItemCfgId
+    if item.ItemCount > Count then
+        item.ItemCount = item.ItemCount - Count
+        return true
+    else
+        Count = Count - item.ItemCount
+        for _, ele in pairs(self._ItemContainer) do
+            if ele.ItemCfgId == cfgId then
+                if ele.ItemCount > Count then
+                    ele.ItemCount = ele.ItemCount - Count
+                    return true
+                else
+                    Count = Count - ele.ItemCount
+                    self._ItemContainer[ele.ItemId] = nil
+                    if Count <= 0 then
+                        return true
+                    end
+                end
+            end
+        end
+    end
 end
 
 return BagClass
--- Simplify/Content/Script/Content/Item/ItemMgr.lua
+++ Simplify/Content/Script/Content/Item/ItemMgr.lua
@@ -1,10 +1,21 @@
 
 ---
----@brief 物件管理器
+---@brief   物件管理器
+---@author zys
+---@data   Thu Aug 28 2025 01:27:23 GMT+0800 (中国标准时间)
 ---
 
+require('Content/Item/ItemBase')
 require('Content/Item/Bag')
 
-class.class"ItemMgr"{
-    
+---@class ItemMgr:MdBase
+local ItemMgr = class.class"ItemMgr": extends 'MdBase'{
+    ctor = function()end,
+    Init = function()end,
 }
+
+function ItemMgr:ctor()
+
+end
+
+return ItemMgr
