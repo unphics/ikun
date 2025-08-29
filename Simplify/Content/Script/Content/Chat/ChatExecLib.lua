86e915b3b8b4521fafee17ff94f9f4625d7a587d
unphics <481381521@qq.com>
2025-08-29 22:54:50 +0800

    小修改

--- Simplify/Content/Script/Content/Chat/ChatExecLib.lua
+++ Simplify/Content/Script/Content/Chat/ChatExecLib.lua
@@ -1,4 +1,10 @@
 
+---
+---@brief   动态对话
+---@author  zys
+---@data    Fri Aug 29 2025 22:54:01 GMT+0800 (中国标准时间)
+---
+
 ---@class ChatExecLib
 local ChatExecLib = {}
 
--- Simplify/Content/Script/Content/Chat/DynaSelect.lua
+++ Simplify/Content/Script/Content/Chat/DynaSelect.lua
@@ -1,4 +1,11 @@
 
+---
+---@brief   动态对话
+---@author  zys
+---@data    Fri Aug 29 2025 22:54:01 GMT+0800 (中国标准时间)
+---
+
+
 local QuestTool = require('Content/Quest/QuestTool')
 local RoleConfig = require('Content/Role/Config/RoleConfig')
 
