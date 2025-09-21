db16c831eed50e0e8e29d5a4979dee56e40f1444
unphics <481381521@qq.com>
2025-09-21 14:18:50 +0800

    配置表Id规格统一

--- Ikun/Content/Script/Ikun/Blueprint/PC/ChatComp.lua
+++ Ikun/Content/Script/Ikun/Blueprint/PC/ChatComp.lua
@@ -20,7 +20,7 @@ end
 ---@public [Server]
 function ChatComp:BeginChat()
     self:S2C_BeginChat()
-    self:_GetNpcChat():NewChat(40011, self:GetOwner().InteractComp.Rep_InteractActor)
+    self:_GetNpcChat():NewChat(400011, self:GetOwner().InteractComp.Rep_InteractActor)
     return true
 end
 
