4e6590d27052e9dad021b3199b8b1ef7c22fc6bd
unphics <481381521@qq.com>
2025-10-18 13:39:06 +0800

    整理代码

--- Ikun/Content/Script/Ikun/UI/UMG/Interact/UI_Gaze.lua
+++ Ikun/Content/Script/Ikun/UI/UMG/Interact/UI_Gaze.lua
@@ -3,36 +3,29 @@
 ---@brief   注视Hud
 ---@author  zys
 ---@data    Wed Aug 27 2025 13:58:38 GMT+0800 (中国标准时间)
----@todo    初始化和反初始化补全
 ---
 
 local EnhInput = require('Ikun/Module/Input/EnhInput')
 local InputMgr = require("Ikun/Module/Input/InputMgr")
 
 ---@class UI_Gaze: UI_Gaze_C
+---@field _GazeInputPower InputPower
 ---@field _PC BP_IkunPC
 local UI_Gaze = UnLua.Class()
 
 ---@override
-function UI_Gaze:Construct()
-    EnhInput.AddIMC(UE.UObject.Load(EnhInput.IMCDef.IMC_Interact))
-end
-
----@override
-function UI_Gaze:Destruct()
-    EnhInput.RemoveIMC(UE.UObject.Load(EnhInput.IMCDef.IMC_Interact))
-end
-
----@override
 function UI_Gaze:OnShow()
-    local powerGaze = InputMgr.BorrowInputPower(self)
-    InputMgr.RegisterInputAction(powerGaze, EnhInput.IADef.IA_Interact, EnhInput.TriggerEvent.Started, self.OnInteractGaze)
+    EnhInput.AddIMC(UE.UObject.Load(EnhInput.IMCDef.IMC_Interact))
+    self._GazeInputPower = InputMgr.BorrowInputPower(self)
+    InputMgr.RegisterInputAction(self._GazeInputPower, EnhInput.IADef.IA_Interact, EnhInput.TriggerEvent.Started, self.OnInteractGaze)
     
     self.TxtGazeName:SetText('')
 end
 
 ---@override
 function UI_Gaze:OnHide()
+    EnhInput.RemoveIMC(UE.UObject.Load(EnhInput.IMCDef.IMC_Interact))
+    InputMgr.UnregisterInputAction(self._GazeInputPower)
 end
 
 ---@override
@@ -40,18 +33,14 @@ function UI_Gaze:Tick(MyGeometry, InDeltaTime)
     self:UpdateGazeInfo()
 end
 
----@private [Show]
+---@private [Show] 如果当前数据端凝视了目标, 那么就显示他的名字
 function UI_Gaze:UpdateGazeInfo()
     local name = self:_GetPC().InteractComp:GetGazeName()
     self.TxtGazeName:SetText(name)
 end
 
----@private [Input]
-function UI_Gaze:InitInput()
-end
 
-
----@private [Op] 玩家想要交互当前注视的物体
+---@private [Op]玩家想要交互当前注视的物体
 function UI_Gaze:OnInteractGaze()
     self:_GetPC().InteractComp:C2S_ReqInteractGaze()
 end
