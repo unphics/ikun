b699fc2e327926db1ca0b882767b4cc902a73760
unphics <481381521@qq.com>
2025-10-18 13:41:30 +0800

    整理代码

--- Ikun/Content/Script/Ikun/UI/UMG/Interact/UI_Chat.lua
+++ Ikun/Content/Script/Ikun/UI/UMG/Interact/UI_Chat.lua
@@ -12,7 +12,7 @@ local InputMgr = require("Ikun/Module/Input/InputMgr")
 ---@field CurInteractId number
 ---@field CurSelectIndex number
 ---@field CurSelectList table[]
----@field PowerInteract InputPower
+---@field _InteractInputPower InputPower
 local UI_Chat = UnLua.Class()
 
 ---@override
@@ -25,26 +25,14 @@ function UI_Chat:Destruct()
     self.BtnTalkNext.OnClicked:Clear()
 end
 
---function UI_Chat:Tick(MyGeometry, InDeltaTime)
---end
-
 ---@override
 function UI_Chat:OnShow()
-    -- local pc = UE.UGameplayStatics.GetPlayerController(self, 0)
-    -- pc.InteractComp:C2S_ReqInteractBegin()
-    self.PowerInteract = InputMgr.ObtainInputPower(self)
-    InputMgr.RegisterInputAction(self.PowerInteract, EnhInput.IADef.IA_BlankSpace,
-        EnhInput.TriggerEvent.Started, self.OnBtnTalkNextClicked)
-    InputMgr.RegisterInputAction(self.PowerInteract, EnhInput.IADef.IA_Wheel,
-        EnhInput.TriggerEvent.Started, self.OnWhellScroll)
-    InputMgr.RegisterInputAction(self.PowerInteract, EnhInput.IADef.IA_Interact,
-        EnhInput.TriggerEvent.Started, self.OnSelectInteract)
-    InputMgr.RegisterInputAction(self.PowerInteract, EnhInput.IADef.IA_Tab,
-        EnhInput.TriggerEvent.Started, self.OnQuitClicked)
+    self._InteractInputPower = InputMgr.ObtainInputPower(self)
+    InputMgr.RegisterInputAction(self._InteractInputPower, EnhInput.IADef.IA_BlankSpace, EnhInput.TriggerEvent.Started, self.OnBtnTalkNextClicked)
+    InputMgr.RegisterInputAction(self._InteractInputPower, EnhInput.IADef.IA_Wheel, EnhInput.TriggerEvent.Started, self.OnWhellScroll)
+    InputMgr.RegisterInputAction(self._InteractInputPower, EnhInput.IADef.IA_Interact, EnhInput.TriggerEvent.Started, self.OnSelectInteract)
+    InputMgr.RegisterInputAction(self._InteractInputPower, EnhInput.IADef.IA_Tab, EnhInput.TriggerEvent.Started, self.OnQuitClicked)
     
-    -- self:SetFocus()
-    -- self:SetKeyboardFocus()
-
     self:ShowSelectPanel(false)
     self.TxtInteractName:SetText('')
     self.CvsTalk:SetVisibility(UE.ESlateVisibility.Hidden)
@@ -52,10 +40,8 @@ end
 
 ---@override
 function UI_Chat:OnHide()
-    -- local pc = UE.UGameplayStatics.GetPlayerController(self, 0)
-    -- pc.InteractComp:C2S_ReqInteractEnd()
-    InputMgr.UnregisterInputAction(self.PowerInteract)
-    InputMgr.ReliquishInputPower(self.PowerInteract)
+    InputMgr.UnregisterInputAction(self._InteractInputPower)
+    InputMgr.ReliquishInputPower(self._InteractInputPower)
 end
 
 ---@public
