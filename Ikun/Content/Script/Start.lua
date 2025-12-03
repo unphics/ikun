acfc70e0522d67e26a1b37989ed99da6638c4fd9
unphics <481381521@qq.com>
2025-12-04 07:46:01 +0800

    小修改

--- Ikun/Content/Script/Start.lua
+++ Ikun/Content/Script/Start.lua
@@ -5,6 +5,8 @@
 ---@data    Sat Apr 05 2025 14:07:42 GMT+0800 (中国标准时间)
 ---
 
+_G.ffi = require 'ffi' ---@type ffilib
+
 do -- 初始化ikf
     local setting = require('Framework/ikf_setting')
     setting.sys_print = _G.IkunLog
