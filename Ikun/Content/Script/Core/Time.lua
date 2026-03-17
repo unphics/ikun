79e2d0e8978adc0e2abed0104112cc191a0dbb0b
unphics <481381521@qq.com>
2026-03-17 20:39:44 +0800

    长期-将旧格式文件头换位新格式文件头

--- Ikun/Content/Script/Core/Time.lua
+++ Ikun/Content/Script/Core/Time.lua
@@ -1,4 +1,16 @@
 
+--[[
+-- -----------------------------------------------------------------------------
+--  Brief       : 时间工具
+--  File        : Time.lua
+--  Author      : zhengyanshuai
+--  Date        : Sat Jan 03 2026 20:28:06 GMT+0800 (中国标准时间)
+--  License     : MIT License
+-- -----------------------------------------------------------------------------
+--  Copyright (c) 2026 zhengyanshuai
+-- -----------------------------------------------------------------------------
+--]]
+
 local ffi = require("ffi")
 
 ffi.cdef[[
@@ -16,10 +28,12 @@ else
     end
 end
 
+---@return number
 local function GetTimestampMS()
-    return tonumber(lib.GetUnixTimestampMS())
+    return tonumber(lib.GetUnixTimestampMS()) or -1
 end
 
+---@return number
 local function GetTimestampSec()
     return GetTimestampMS() / 1000
 end
