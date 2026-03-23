142354f4b5acda48484d5eda3f7faceeba2420b0
unphics <481381521@qq.com>
2026-03-23 12:51:32 +0800

    UI整理

--- Ikun/Content/Script/Ikun/UI/UMG/MainHud/UI_MainHud.lua
+++ Ikun/Content/Script/Ikun/UI/UMG/MainHud/UI_MainHud.lua
@@ -1,11 +1,16 @@
 
----
----@brief   主界面
----@author  zys
----@data    Sat Apr 05 2025 15:27:02 GMT+0800 (中国标准时间)
----
+--[[
+-- -----------------------------------------------------------------------------
+--  Brief       : UI-主界面
+--  File        : UI_MainHud.lua
+--  Author      : zhengyanshuai
+--  Date        : Sat Apr 05 2025 15:27:02 GMT+0800 (中国标准时间)
+--  License     : MIT License
+-- -----------------------------------------------------------------------------
+--  Copyright (c) 2025-2026 zhengyanshuai
+-- -----------------------------------------------------------------------------
+--]]
 
-local log =  require("Core/Log/log")
 local UnLuaClass = require("Core/UnLua/Class")
 
 ---@class UI_MainHud: UI_MainHud_C
@@ -28,31 +33,6 @@ end
 ---@private
 function UI_MainHud:UpdateTimeInfo()
     self.TxtTime:SetText(TimeMgr:GetCurTimeDisplay())
-end
-
----@private [Debug] 更新动画蓝图的信息
-function UI_MainHud:UpdateAnimInfo()
-    local chr = UE.UGameplayStatics.GetPlayerCharacter(self, 0) ---@type BP_ChrBase
-    if not obj_util.is_valid(chr) then
-        return
-    end
-    local animInst = chr.Mesh:GetAnimInstance()
-    local velBlend = animInst.VelBlend
-    local str = ''
-    if velBlend then
-        str = 'VelBlend:\n'
-        str = str..'    F: '..log.fmt54(velBlend.F)..'\n'
-        str = str..'    L: '..log.fmt54(velBlend.L)..'\n'
-        str = str..'    R: '..log.fmt54(velBlend.R)..'\n'
-        str = str..'    B: '..log.fmt54(velBlend.B)..'\n'
-    end
-    local EMoveDir = animInst.MoveDir
-    if EMoveDir then
-        local name = {'Front', 'Back', 'Left', 'Right'}
-        str = str..'MoveDir: '..tostring(name[EMoveDir + 1] or 'nil')..'\n'
-    end
-    str = str .. 'PullingBow: ' .. (animInst.bPullingBow and 'true' or 'false')
-    self.TxtAnim:SetText(str)
 end
 
 return UI_MainHud
