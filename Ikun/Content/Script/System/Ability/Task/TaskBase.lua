bc21aac13676ee1c2dc962ae9f0b319cf411c2a1
unphics <481381521@qq.com>
2026-04-12 22:28:08 +0800

    TaskGraph

--- Ikun/Content/Script/System/Ability/Task/TaskBase.lua
+++ Ikun/Content/Script/System/Ability/Task/TaskBase.lua
@@ -1,28 +1,74 @@
 
+--[[
+-- -----------------------------------------------------------------------------
+--  Brief       : TaskBaseClass
+--  File        : TaskBase.lua
+--  Author      : zhengyanshuai
+--  Date        : Sun Apr 12 2026 12:39:03 GMT+0800 (中国标准时间)
+--  Description : 任务实例
+--  License     : MIT License
+-- -----------------------------------------------------------------------------
+--  Copyright (c) 2026 zhengyanshuai
+-- -----------------------------------------------------------------------------
+--]]
 
 local Class3 = require("Core/Class/Class3")
 local log = require("Core/Log/log")
 local Delegate = require("Core/Delegate")
 
 ---@class TaskBaseClass
+---@field private __Owner any
+---@field private __TaskGraph TaskGraphClass?
 local TaskBaseClass = Class3.Class("TaskBaseClass")
 
 function TaskBaseClass:Ctor(InOwner)
+    self.__Owner = InOwner
 end
 
+---@public
 function TaskBaseClass:BeginTask()
 end
 
+---@public
 function TaskBaseClass:EndTask()
+    local graph = self:GetTaskGraph()
+    if graph then
+        graph:NotifyTaskEnd(self)
+    end
 end
 
-function TaskBaseClass:SetTaskGraph()
+---@public
+function TaskBaseClass:TickTask(DeltaTime)
 end
 
-function TaskBaseClass:GetTaskGraph()
+---@public
+---@return boolean
+function TaskBaseClass:IsTaskRunning() -- const
+    return false
 end
 
-function TaskBaseClass:SendEventToGraph()
+---@public
+---@param InTaskGraph TaskGraphClass?
+function TaskBaseClass:SetTaskGraph(InTaskGraph)
+    self.__TaskGraph = InTaskGraph
+end
+
+---@public
+---@return TaskGraphClass?
+function TaskBaseClass:GetTaskGraph() -- const
+    return self.__TaskGraph
+end
+
+---@public
+function TaskBaseClass:SendEventToGraph(InEvent)
+    if not self:GetTaskGraph() then
+        return
+    end
+end
+
+---@public
+function TaskBaseClass:GetTaskOwner()
+    return self.__Owner
 end
 
 return TaskBaseClass
--- Ikun/Content/Script/System/Ability/Task/TaskGraph.lua
+++ Ikun/Content/Script/System/Ability/Task/TaskGraph.lua
@@ -1,4 +1,16 @@
 
+--[[
+-- -----------------------------------------------------------------------------
+--  Brief       : TaskGraphClass
+--  File        : TaskGraph.lua
+--  Author      : zhengyanshuai
+--  Date        : Sun Apr 12 2026 21:09:36 GMT+0800 (中国标准时间)
+--  Description : 任务图
+--  License     : MIT License
+-- -----------------------------------------------------------------------------
+--  Copyright (c) 2026 zhengyanshuai
+-- -----------------------------------------------------------------------------
+--]]
 
 local Class3 = require("Core/Class/Class3")
 local log = require("Core/Log/log")
@@ -9,27 +21,52 @@ local TaskGraphClass = Class3.Class("TaskGraph")
 
 function TaskGraphClass:Ctor(InOwner)
     self._Owner = InOwner
+    self.DelayTasks = {}
+    self.FollowTasks = {}
+    self.WaitTasks = {}
+    self.RunningTasks = {}
 end
 
 function TaskGraphClass:BeginTaskGraph()
 end
 
-function TaskGraphClass:TickTaskGraph()
-end
-
 function TaskGraphClass:EndTaskGraph()
 end
 
-function TaskGraphClass:AddDelayTask()
+function TaskGraphClass:TickTaskGraph(DeltaTime)
 end
 
-function TaskGraphClass:AddSequenceTask()
+---@public
+function TaskGraphClass:AddDelayTask(InDelayTime, InDelayTask)
+    table.insert(self.DelayTasks, {Task = InDelayTask, DelayTime = InDelayTime})
 end
 
-function TaskGraphClass:AddWaitEventTask()
+function TaskGraphClass:AddFollowTask(InPreTask, InNextTask)
+    local tasks = self.FollowTasks[InPreTask]
+    if not tasks then
+        tasks = {}
+        self.FollowTasks[InPreTask] = tasks
+    end
+    table.insert(tasks, InNextTask)
 end
 
-function TaskGraphClass:IsAnyTaskRunning()
+function TaskGraphClass:AddWaitEventTask(InEvent, InWaitTask)
+    local tasks = self.WaitTasks[InEvent]
+    if not tasks then
+        tasks = {}
+        self.WaitTasks[InEvent] = tasks
+    end
+    table.insert(tasks, InWaitTask)
+end
+
+---@public
+---@return boolean
+function TaskGraphClass:IsAnyTaskRunning() -- const
+    return #self.RunningTasks > 0
+end
+
+---@public
+function TaskGraphClass:NotifyTaskEnd(InTask)
 end
 
 return TaskGraphClass
