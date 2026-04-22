26b50bb5e6d2392c3a64c30cf5c9c53f0ec08b20
unphics <481381521@qq.com>
2026-04-22 22:30:48 +0800

    功能: 再次整理配置解析系统

--- Ikun/Content/Script/System/Ability/Attr/AttrConfig.lua
+++ Ikun/Content/Script/System/Ability/Attr/AttrConfig.lua
@@ -18,6 +18,7 @@ local FileSystem = require("System/File/FileSystem")
 local log = require('Core/Log/log')
 local ExpLib = require("System/Ability/Exp/ExpLib")
 local TagUtils = require("System/Ability/Tag/TagUtils")
+local AttrDef = require("System/Ability/Attr/AttrDef")
 
 ---@alias AttrFormulaFunction fun(Attributes: table<integer, number>):number
 ---@alias AttrImposeFormulaFunction fun(SourceAttribute: table<integer, number>, TargetAttribute: table<integer, number>):number
@@ -44,12 +45,23 @@ local ATTRSET_CONFIG_PATH = "Ability/Attr/Set.csv"
 ---@class AttrConfigClass
 ---@field protected __AttrConfig table<string, AttrConfig>
 ---@field protected __AttrSetConfig table<string, AttrSetConfig>
+---@field protected _AttrFormula table<integer, AttrFormulaFunction>
+---@field protected _AttrReceiveFormula table<integer, AttrReceiveFormulaFunction>
+---@field protected _AttrDependencies table<integer, integer[]> (属性, 该属性依赖的属性[]) 依赖查找表, 我依赖谁
+---@field protected _AttrDependents table<integer, integer[]> (属性, 依赖该属性的属性[]) 反向依赖查找表, 谁依赖我
 local AttrConfigClass = Class3.Class("AttrConfigClass")
+
+function AttrConfigClass:Ctor()
+end
 
 ---@public
 function AttrConfigClass:LoadAttrConfigs()
     self:__LoadAttrConfig()
     self:__LoadAttrSetConfig()
+    self:__BuildAttrDependencies()
+    self:__BuildAttrDependents()
+    self:__BuildAttrFormulas()
+    self:__BuildAttrReceiveFormulas()
 end
 
 ---@private
@@ -72,6 +84,97 @@ function AttrConfigClass:__LoadAttrSetConfig()
     local attrSetConfig = setParser:ToRows():ExtractHeaders():ToGrid():ToMap():CastArrCol({"SetAttrs"}):GetResult()
     setParser:ReleaseParser()
     self.__AttrSetConfig = attrSetConfig
+end
+
+---@private
+function AttrConfigClass:__BuildAttrDependencies()
+    if not self.__AttrConfig or not next(self.__AttrConfig) then
+        return
+    end
+    local attrDeps = {} ---@type table<string, string[]>
+
+    for key, _ in pairs(self.__AttrConfig) do
+        if not attrDeps[key] then
+            attrDeps[key] = {}
+        end
+    end
+
+    ---@param config AttrConfig
+    for key, config in pairs(self.__AttrConfig) do
+        local deps = ExpLib.CollectDeps(config.AttrFormula)
+        if deps then
+            for _, src in ipairs(deps) do
+                if not attrDeps[key] then
+                    attrDeps[key] = {}
+                end
+                table.insert(attrDeps[key], src)
+            end
+        end
+    end
+
+    local sorted = ExpLib.TopoSortDFS(attrDeps) ---@type string[]
+    for i = #sorted, 1, -1 do
+        AttrDef.Attr[sorted[i]] = #sorted - i + 1
+    end
+    AttrDef.BuildIdToKey()
+    
+    local attrNumDeps = {}
+    for key, deps in pairs(attrDeps) do
+        local tb = {}
+        for _, dep in ipairs(deps) do
+            table.insert(tb, AttrDef.Attr[dep])
+        end
+        attrNumDeps[AttrDef.Attr[key]] = tb
+    end
+    
+    self.__AttrDependencies = attrNumDeps
+end
+
+function AttrConfigClass:__BuildAttrDependents()
+    if not self.__AttrDependencies or not next(self.__AttrDependencies) then
+        return
+    end
+    local attrDependents = {}
+    for attr, deps in pairs(self.__AttrDependencies) do
+        for _, dep in ipairs(deps) do
+            if not attrDependents[dep] then
+                attrDependents[dep] = {}
+            end
+            table.insert(attrDependents[dep], attr)
+        end
+    end
+    self.__AttrDependents = attrDependents
+end
+
+---@private
+function AttrConfigClass:__BuildAttrFormulas()
+    if not self.__AttrConfig or not next(self.__AttrConfig) then
+        return
+    end
+    local attrFormula = {}
+    ---@param config AttrConfig
+    for key, config in pairs(self.__AttrConfig) do
+        local func = ExpLib.CompileAttrFormula(config.AttrFormula)
+        if func then
+            attrFormula[AttrDef.Attr[key]] = func
+        end
+    end
+    self.__AttrFormula = attrFormula
+end
+
+---@private
+function AttrConfigClass:__BuildAttrReceiveFormulas()
+    if not self.__AttrConfig or not next(self.__AttrConfig) then
+        return
+    end
+    local receiveFormula = {}
+    for key, config in pairs(self.__AttrConfig) do
+        local func = ExpLib.CompileAttrReceiveFormula(config.AttrReceiveFormula)
+        if func then
+            receiveFormula[AttrDef.Attr[key]] = func
+        end
+    end
+    self.__AttrReceiveFormula = receiveFormula
 end
 
 return AttrConfigClass
